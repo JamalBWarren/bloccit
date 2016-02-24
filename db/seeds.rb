8fc82413a8bd8d68419804eaae157c6512d8f0c3
jamalbwarren <jamalbwarren@gmail.com>
2016-02-24 23:14:35 +0000

    assignment-30-complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,6 +17,10 @@ end
             post: posts.sample,
             body: RandomData.random_paragraph)
 end
+
+uniqpost = Post.find_or_create_by(title: "This is a unique title", body:"this is a unique body")
+Comment.find_or_create_by(body: "unique comment body", post: uniqpost)
+
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
